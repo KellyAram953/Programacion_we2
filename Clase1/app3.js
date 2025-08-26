209402ebdf00d2494ba26433c57d4205ee0762c2
Key <kellyaramburo953@aragon.unam.mx>
2025-08-26 10:24:02 -0600

    Clase1
    Ejercicio en clase

---
+++ Clase1/app3.js
@@ -0,0 +1,24 @@
+let saludo = "HOLA";
+let saludo2 = "Como estas";
+
+let unir = saludo + saludo2;
+
+console.log(unir);
+//Otra forma de hacerlo
+console.log(saludo + saludo2);
+
+let usuario;
+let edad;
+let nacionalidad;
+let noCuenta;
+let estatura;
+
+usuario = "Kelly";
+edad = 20;
+nacionalidad = "Mexicana";
+noCuenta = "320269953";
+estatura = "1.56";
+
+console.log("Mi ombre es" + usuario + " Mi edad es" + edad + "Mi nacionalidad es" + nacionalidad + 
+    "Mi numero de cuenta es" + noCuenta + "mido" + estatura)
+
---
+++ Clase1/app5.js
@@ -0,0 +1,14 @@
+let variable1 = parseInt(prompt("Ingresa el numero uno")) ;
+let variable2 = parseInt (prompt("Ingresa el numero dos"));
+
+// let variablesuma = parseInt(prompt(variable1 + variable2));
+// let variableresa = parseInt(prompt(variable1 - variable2));
+// let variablemultiplicacion = parseInt(prompt(variable1 * variable2));
+// let variabledivision  = parseInt(prompt(variable1 / variable2));
+
+
+console.log(variable1 + variable2);
+console.log(variable1 - variable2);
+console.log(variable1 * variable2);
+console.log(variable1 / variable2);
+
