55824041434fa126294e069e8fa7186a02646ff3
Key <kellyaramburo953@aragon.unam.mx>
2025-10-30 15:07:39 -0600

    Clase2

---
+++ Clase2/app3.js
@@ -0,0 +1,5 @@
+let clave = prompt("Ingresa la clave secreta");
+if(clave == "1234"){
+    console.log("Exito");
+}
+// console.log("Continua el flujo");
---
+++ Clase2/app4.js
@@ -0,0 +1,6 @@
+let port =3008;
+if(port){
+    console.log("Puerto correcto");
+}else{
+    console.log("Error en puerto");
+}
