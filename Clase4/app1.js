d856937cfad7386189eaaad94cb644157c271ff8
Key <kellyaramburo953@aragon.unam.mx>
2025-10-30 15:08:41 -0600

    Clasee4

---
+++ Clase4/app1.js
@@ -0,0 +1,25 @@
+/*var estado = true;
+if(estao){
+    var estado =false;
+}
+console.long(estado);*/
+
+/*let estado = true;
+if(estado){
+    let estado = false;
+}
+console.log(estado);*/
+
+//se imprime 
+/*for(let i=0; i<=10; i++){
+    console.log(i);
+}
+console.log(i);*/
+
+const array=[];
+array = ["Hola"];
+console.log(array);
+
+const miarray=[];
+miarray [0] =["Hola"];
+console.log(miarray);
